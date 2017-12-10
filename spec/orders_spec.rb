3619528ec165b70aa32cc985cc0ec3853fa3ccfb
Kris <kris.weber.1994@gmail.com>
2017-12-10 15:33:44 -0800

    Add expect statements for order controller tests and sessions controller tests

--- spec/orders_spec.rb
+++ spec/orders_spec.rb
@@ -3,25 +3,52 @@
 require 'spec_helper'
 
 describe 'Order Controller' do
-  context 'get /orders/new route' do
-    it 'should respond successfully to the /orders/new route' do
-      get '/orders/new'
-      expect(last_response.status).to eq(200)
+
+  xcontext 'get /orders route' do
+
+    context 'valid user' do
     end
 
-    it 'should display a form' do
-      get '/orders/new'
-      expect(last_response.body).to include('<form id=\'order-form\'')
+    context 'invalid user' do
     end
 
   end
 
-  context 'post /orders route' do
+  xcontext 'get /orders/new route' do
+
+    context 'valid user' do
+      it 'should respond successfully to the /orders/new route' do
+        get '/orders/new'
+        expect(last_response.status).to eq(200)
+      end
+
+      it 'should display a form' do
+        get '/orders/new'
+        expect(last_response.body).to include('<form id=\'order-form\'')
+      end
+    end
+
+    context 'invalid user' do
+      it 'should redirect' do
+        get '/orders/new'
+        expect(last_response.status).to eq(302)
+      end
+
+      it 'should redirect to /sessions/new' do
+        get '/orders/new'
+        expect(last_response.location).to include('/sessions/new')
+      end
+    end
+  end
+
+  xcontext 'post /orders route' do
+
     context 'valid order' do
       it 'should redirect after submission' do
         post '/orders'
         expect(last_response.status).to eq(302)
       end
+
       it 'should redirect to /orders after submission' do
         post '/orders'
         expect(last_response.location).to include('/orders')
@@ -41,7 +68,6 @@ describe 'Order Controller' do
 
       it 'should include the error message' do
       end
-
     end
 
     it 'should redirect if not logged in' do
@@ -51,8 +77,6 @@ describe 'Order Controller' do
     it 'should redirect to /sessions/new if not logged in' do
       expect(last_response.location).to include('/sessions/new')
     end
-
-
   end
 
 end
--- spec/sessions_spec.rb
+++ spec/sessions_spec.rb
@@ -18,6 +18,25 @@ decribe 'Sessions Controller' do
   end
 
   context 'delete /sessions/:id' do
+    before(:each) do
+      # post '/sessions' with valid user email
+    end
+
+    it 'should redirect' do
+      delete '/sessions/1'
+      expect(last_response.status).to eq(302)
+    end
+
+    it 'should redirect to /sessions/new' do
+      delete '/sessions/1'
+      expect(last_response.location).to include('/sessions/new')
+    end
+
+    it 'should clear the session' do
+      session = {}
+      delete '/sessions/1', 'rack.session' => session
+      expect(session[:user_id]).to be nil
+    end
   end
 
 end
