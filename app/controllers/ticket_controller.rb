80d50863cf7cf70510c5fa03e347e4af04a6e7d9
Kris <kris.weber.1994@gmail.com>
2017-12-20 08:18:22 -0800

    Add views for influencer_orders, assign ticket id to new influencer orders

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -1,11 +1,19 @@
 post '/tickets' do
-  @influencer_orders = InfluencerOrder.all
+  influencer_orders = InfluencerOrder.all
 
   @ticket = Ticket.new
   @ticket.assign_filename
+  @ticket.save
+
+  influencer_orders.each do |order|
+    if order.ticket_id != nil
+      order.ticket_id = @ticket.id
+    end
+  end
+
+  puts "Ticket filename is " + @ticket.filename
+  monthly_csv = create_csv_file(@ticket.filename)
 
   content_type('application/csv')
-  attachment(@ticket.filename)
-
-  generate_ticket
+  attachment(monthly_csv)
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ActiveRecord::Base
 
   has_many :influencer_orders
 
-  header_mapping = ["order_number","groupon_number","order_date","merchant_sku","quantity_requested","shipment_method_requested","shipment_address_name","shipment_address_street","shipment_address_street_2","shipment_address_city","shipment_address_state","shipment_address_postal_code","shipment_address_country","gift","gift_message","quantity_shipped","shipment_carrier","shipment_method","shipment_tracking_number","ship_date","groupon_sku","custom_field_value","permalink","item_name","vendor_id","salesforce_deal_option_id","groupon_cost","billing_address_name","billing_address_street","billing_address_city","billing_address_state","billing_address_postal_code","billing_address_country","purchase_order_number","product_weight","product_weight_unit","product_length","product_width","product_height","product_dimension_unit","customer_phone","incoterms","hts_code","3pl_name","3pl_warehouse_location","kitting_details","sell_price","deal_opportunity_id","shipment_strategy","fulfillment_method","country_of_origin","merchant_permalink","feature_start_date","feature_end_date","bom_sku","payment_method","color_code","tax_rate","tax_price"]
+  header_arr = ["order_number","groupon_number","order_date","merchant_sku_item","quantity_requested","shipment_method_requested","shipment_address_name","shipment_address_street","shipment_address_street_2","shipment_address_city","shipment_address_state","shipment_address_postal_code","shipment_address_country","gift","gift_message","quantity_shipped","shipment_carrier","shipment_method","shipment_tracking_number","ship_date","groupon_sku","custom_field_value","permalink","item_name","vendor_id","salesforce_deal_option_id","groupon_cost","billing_address_name","billing_address_street","billing_address_city","billing_address_state","billing_address_postal_code","billing_address_country","purchase_order_number","product_weight","product_weight_unit","product_length","product_width","product_height","product_dimension_unit","customer_phone","incoterms","hts_code","3pl_name","3pl_warehouse_location","kitting_details","sell_price","deal_opportunity_id","shipment_strategy","fulfillment_method","country_of_origin","merchant_permalink","feature_start_date","feature_end_date","bom_sku","payment_method","color_code","tax_rate","tax_price"]
 
   def self.assign_filename
     current_date = DateTime.now
@@ -10,90 +10,77 @@ class Ticket < ActiveRecord::Base
   end
 
   def create_csv_file(filename)
-    @influencer_orders = InfluencerOrder.all
-    column_header = header_mapping
+    ticket = Ticket.find_by(filename: filename)
+    @influencer_orders = ticket.influencer_orders
+    column_header = header_arr
     CSV.open(filename,'w',:write_headers => true,
     :headers => column_header) do |header|
       column_header = nil
-      @influencer_orders.each do |row|
-        data_out = [order.order_number,
-          "",
-          "",
-          "",
-          "",
-          "",
-          order.influencer.first_name + " " + order.influencer.last_name,
-          order.influencer.address1,
-          order.influencer.city,
-          order.influencer.state,
-          order.influencer.zip,
-          "US",
-          "FALSE",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          ITEMNAME,
-          "",
-          "",
-          "",
-          order.influencer.first_name + " " + order.influencer.last_name,
-          order.influencer.address1,
-          order.influencer.city,
-          order,influencer.state,
-          order.influencer.zip,
-          "US",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          order.influencer.phone,
-          "",
-          "",
-          "",
-          "",
-          "",
-          SELLPRICE,
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "" + "\n"
-        ]
-        row << data_out
+      @influencer_orders.each do |order|
+        order.line_items.each do |item|
+
+          data_out = [order.order_number,
+            "",
+            "",
+            "",
+            item.sku,
+            "",
+            order.influencer.first_name + " " + order.influencer.last_name,
+            order.influencer.address1,
+            order.influencer.city,
+            order.influencer.state,
+            order.influencer.zip,
+            "US",
+            "FALSE",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            item.title,
+            "",
+            "",
+            "",
+            order.influencer.first_name + " " + order.influencer.last_name,
+            order.influencer.address1,
+            order.influencer.city,
+            order,influencer.state,
+            order.influencer.zip,
+            "US",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            order.influencer.phone,
+            "",
+            "",
+            "",
+            "",
+            "",
+            item.price,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "" + "\n"
+          ]
+          order << data_out
+        end
       end
-    end
-  end
-
-  def write_data_to_csv(filename)
-    @influencers = Influencer.all
-    @influencer_orders = InfluencerOrder.all
-    File.open(filename,'a+') do |header|
-      if header.tell() == 0
-      end
-
-    end
-  end
-
-  def generate_ticket(records)
-    CSV.generate do |csv|
-      csv << Ticket.attribute_names
-      records.each { |r| csv << r.attributes.values }
     end
   end
 
