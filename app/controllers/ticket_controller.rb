a9d5297f798fcfb4b031ccf59ab65e2ccb722f8f
Kris <kris.weber.1994@gmail.com>
2017-12-30 14:29:29 -0800

    Redirect after Order creation, download CSV from ticket#show page

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -3,9 +3,16 @@ require_relative './order_controller'
 
 get '/tickets/:id' do
   @ticket = Ticket.find(params[:id])
-  # @influencers = Influencer.all
-  # @line_items = LineItem.all
-  # erb :'tickets/show', locals: { ticket: @ticket, influencers: @influencers, line_items: @line_items }
+
+  erb :'tickets/show', locals: { ticket: @ticket }
+
+
+  # create_csv_file(@ticket.id)
+  # send_file(@ticket.filename, :filename => @ticket.filename)
+end
+
+get '/tickets/:id/download' do
+  @ticket = Ticket.find(params[:id])
   p @ticket.filename
   create_csv_file(@ticket.id)
   send_file(@ticket.filename, :filename => @ticket.filename)
